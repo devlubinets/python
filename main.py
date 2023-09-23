2139c53bf631d46d33525400acea6a63a717477f
Lubynets Kyrylo <dev.lubinets@gmail.com>
2023-09-23 21:34:56 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+handle = open("/home/ad/WebProjects/dev/test/testData/textFile.txt")
+fileContent = handle.read()
+print("______________________________________")
+print(fileContent)
