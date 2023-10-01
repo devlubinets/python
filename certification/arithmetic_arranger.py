848444fcd4b4fc760cb911e6fb6ab0651b3089f2
Lubynets Kyrylo <dev.lubinets@gmail.com>
2023-10-01 13:05:01 +0100

    in progress

--- certification/arithmetic_arranger.py
+++ certification/arithmetic_arranger.py
@@ -1,29 +1,53 @@
 import re
 
+
 def arithmetic_arranger(problems, calc=False):
-  if (len(problems) > 5):
-    return "Error: Too many problems"
+    arranged_problems = dict()
 
-  arranged_problems = list()
+    if (int(len(problems)) > 5):
+        return "Error: Too many problems."
 
-  for p in problems:
-    numbers = re.findall("(\d+)", p)
-    operator = re.findall("[+-]", p)
-    result = ""
+    for p in problems:
+        numbers = re.findall("(\d+)", p)
 
-    line = "-" * (len(max(numbers)) + 2)
+        if not all(n.isdigit() for n in numbers):
+            return "Error: Numbers must only contain digits."
 
-    if calc:
-      if operator == "+":
-        result = numbers[0] + numbers[1]
-      if operator == "-":
-        result = numbers[0] - numbers[1]
+        if int(len(numbers[0])) > 4 or int(len(numbers[1])) > 4:
+            return "Error: Numbers cannot be more than four digits."
 
-    arranged_problems.append(
-        f"{numbers[0]}\n {operator}\n {numbers[1]}\n {line}\n {result}"
-    )
+        operator = re.findall("[+-]", p)
 
-  return arranged_problems
+        if len(operator) == 0:
+            return "Error: Operator must be '+' or '-'."
 
-r = arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+        result = ""
+        line = "-" * (len(max(numbers)) + 2)
+
+        if calc:
+            if operator == "+":
+                result = numbers[0] + numbers[1]
+            if operator == "-":
+                result = numbers[0] - numbers[1]
+
+        arranged_problems["x"] = numbers[0]
+        arranged_problems["y"] = f"{operator} {numbers[1]}"
+        arranged_problems["line"] = line
+        arranged_problems["result"] = result
+
+    arranged = list()
+
+    for x in arranged_problems["x"]:
+        arranged.append(x)
+    for y in arranged_problems["y"]:
+        arranged.append(y)
+    for line in arranged_problems["line"]:
+        arranged.append(line)
+    for result in arranged_problems["result"]:
+        arranged.append(result)
+
+    return arranged_problems
+
+
+r = arithmetic_arranger(["3801 - 2", "123 + 49"])
 print(r)
